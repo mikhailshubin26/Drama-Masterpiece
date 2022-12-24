245600cabd9d32bec80e99d312349224a4c8c429
Mikhail <94745917+mikhailshubin26@users.noreply.github.com>
2022-12-24 13:48:50 +0300

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,15 @@
+package com.example.quest;
+
+public class Character {
+    public int K;
+    public int A;
+    public int R;
+    public String name;
+
+    public Character(String name) {
+        K = 1;
+        A = 100;
+        R = 50;
+        this.name = name;
+    }
+}
